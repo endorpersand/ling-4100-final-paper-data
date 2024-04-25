4f846591b2e4f65a860222769607b9991a81ef40
Henry Bui <henry.h.bui05@gmail.com>
2024-04-25 18:53:39 -0400

    get rid of things i don't need

--- backend/src/dal/psa.ts
+++
@@ -1,7 +0,0 @@
-import * as db from "../init/db";
-
-type PSA = MonkeyTypes.WithObjectId<SharedTypes.PSA>;
-
-export async function get(): Promise<PSA[]> {
-  return await db.collection<PSA>("psa").find().toArray();
-}
--- docker/backend/entry-point.sh
+++
@@ -1,3 +0,0 @@
-#!bin/sh
-./applyConfig.sh &
-node server.js
